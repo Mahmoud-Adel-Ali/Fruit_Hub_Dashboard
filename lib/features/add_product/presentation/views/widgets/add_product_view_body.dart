67f546aa99a955676c5fe2a43a10ac51f6b2972a
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-09-27 22:15:23 -0700

    121. Create add product form part one

--- lib/features/add_product/presentation/views/widgets/add_product_view_body.dart
+++ lib/features/add_product/presentation/views/widgets/add_product_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../core/widgets/custom_text_form_field.dart';
+
 class AddProductViewBody extends StatefulWidget {
   const AddProductViewBody({super.key});
 
@@ -17,7 +19,32 @@ class _AddProductViewBodyState extends State<AddProductViewBody> {
       child: Form(
         key: formKey,
         autovalidateMode: autovalidateMode,
-        child: Column(),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            spacing: 16,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomTextFormField(
+                hintText: 'name',
+                keyboardType: TextInputType.text,
+              ),
+              CustomTextFormField(
+                hintText: 'price',
+                keyboardType: TextInputType.number,
+              ),
+              CustomTextFormField(
+                hintText: 'code',
+                keyboardType: TextInputType.number,
+              ),
+              CustomTextFormField(
+                hintText: 'description',
+                keyboardType: TextInputType.text,
+                maxLines: 5,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
