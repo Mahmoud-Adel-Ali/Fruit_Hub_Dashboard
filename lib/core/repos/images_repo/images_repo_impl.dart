8f16b43c9d86418b45a1f96fa98bff19cd50b4c3
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-10-25 09:46:02 -0700

    132. Implement upload file part two.

--- lib/core/repos/images_repo/images_repo_impl.dart
+++ lib/core/repos/images_repo/images_repo_impl.dart
@@ -1,14 +1,29 @@
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:dartz/dartz.dart';
+import 'package:fruit_hub_dashboard/core/services/storage_service.dart';
 
 import '../../errors/failures.dart';
+import '../../utils/end_points.dart';
 import 'images_repo.dart';
 
 class ImagesRepoImpl implements ImagesRepo {
+  final StorageService storageService;
+
+  ImagesRepoImpl({required this.storageService});
+
   @override
-  Future<Either<Failure, String>> uploadImage({required File image}) {
-    // TODO: implement uploadImage
-    throw UnimplementedError();
+  Future<Either<Failure, String>> uploadImage({required File image}) async {
+    try {
+      final imageUrl = await storageService.uploadFile(
+        file: image,
+        path: EndPoints.images,
+      );
+      return Right(imageUrl);
+    } catch (e) {
+      log("Failed to upload image: $e");
+      return Left(ServerFailure("Failed to upload image."));
+    }
   }
 }
